3ba2c15ee579de7e429bb24a1fd42e1c90965dff
Benjamin Martin <benjaminmartin@theitguy.me>
2020-04-27 02:04:56 -0700

    Latest version with iteration through emails

--- cryptobot.py
+++ cryptobot.py
@@ -2,22 +2,25 @@ import requests
 import configparser
 import json
 import logging
-import os
-import sys
-import smtplib, ssl
-from socket import gaierror
+import smtplib
 import time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-## Gets email information from password.json file
+## Gets email information
 def get_details():
 
-    f = open('password.json',)
+    ## Receiver hosts each email that will be sent out
+    ## Will need to replace email and sender with your own
+    f = '{"password": "x", "sender": "y","receiver": []}'
 
-    data = json.load(f)
+    ## loads is used for string. Removing the s if not a string anymore
+    data = json.loads(f)
 
-    f.close()
+    ## Repeat per email that is supposed to be sent out
+    ## Could put this list in a file, but gotta figure out how to do it with pyinstaller
+    data["receiver"].append("a")
+    data["receiver"].append("b")
 
     return data
 
@@ -28,25 +31,32 @@ def email(percent):
 
     email = data['sender']
     password = data['password']
-    send_to_email = data['receiver']
-    subject = 'CryptoBot has detected a drop in Bitcoin' # The subject line
-    message = """\
-    BITCOIN HAS DROPPED BY """ + str(percent) + """% AND YOU NEED TO START BUYING!!!"""
-
-    msg = MIMEMultipart()
-    msg['From'] = email
-    msg['To'] = send_to_email
-    msg['Subject'] = subject
-
-    # Attach the message to the MIMEMultipart object
-    msg.attach(MIMEText(message, 'plain'))
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login(email, password)
-    text = msg.as_string() # You now need to convert the MIMEMultipart object to a string to send
-    server.sendmail(email, send_to_email, text)
+
+    ## Iterates through each email and attempts to send a notification
+    for receiver in data['receiver']:
+        send_to_email = receiver
+        subject = 'CryptoBot has detected a drop in Bitcoin' # The subject line
+        message = """\
+        BITCOIN HAS DROPPED BY """ + str(percent) + """% AND YOU NEED TO START BUYING!!!"""
+
+        msg = MIMEMultipart()
+        msg['From'] = email
+        msg['To'] = send_to_email
+        msg['Subject'] = subject
+
+        # Attach the message to the MIMEMultipart object
+        msg.attach(MIMEText(message, 'plain'))
+        server.login(email, password)
+        text = msg.as_string() # You now need to convert the MIMEMultipart object to a string to send
+        server.sendmail(email, send_to_email, text)
+        logging.info('Email sent to - ' + send_to_email)
+
     server.quit()
+
+    return
 
 ## Simple function to determine if a three_candle_drop (probably) happened ¯\_(ツ)_/¯
 def three_candle_drop(data):
@@ -95,6 +105,7 @@ def get_candle_data(config, session):
 
 ## What is run upon execution
 if __name__ == '__main__':
+
     logging.basicConfig(level=logging.DEBUG)
 
     config = configparser.ConfigParser()
@@ -110,6 +121,7 @@ if __name__ == '__main__':
             if percent != False:
                 logging.info('Sending email!')
                 email(percent)
+                logging.info('Emails have been sent')
             else:
                 logging.info('Carry on...')
         else :
